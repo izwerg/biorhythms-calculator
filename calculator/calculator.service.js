d4ba0ec682677d50842eed8bb4157fa981b262e5
Andrew Trubchanin <andrew.trubchanin@gmail.com>
2018-11-23 20:10:36 +0300

    Simplify calculateBiorhythms factory

--- calculator/calculator.service.js
+++ calculator/calculator.service.js
@@ -3,25 +3,22 @@
 myApp.factory('calculateBiorhythms', function() {
   return function(days) {
     var biorhythmsData = [];
-    var now = new Date();
     var thirtyDays = Date.now() + (86400000 * 30);
-    var biorhythm = [23, 28, 33];
-    var biorName = ['physical', 'emotional', 'intellectual'];
 
-    if (!biorhythmsData[0]) {
-      for (var x = Date.now(); x < thirtyDays; x += 86400000) {
-        var day = new Date(x);
-        biorhythmsData.push({date: day, physical: null, emotional: null, intellectual: null});
-      }
-    }
-
-    for (var n = 0; n < 30; n++) {
-      for (var k = 0; k < 3; k++) {
-        biorhythmsData[n][biorName[k]] = Math.round(Math.sin(2 * Math.PI * (days / biorhythm[k])) * 100);
-      }
-      days += 1;
+    for (var x = Date.now(); x < thirtyDays; x += 86400000) {
+      biorhythmsData.push({
+        date: new Date(x),
+        physical:  calcBioFormula(days, 23),
+        emotional: calcBioFormula(days, 28),
+        intellectual: calcBioFormula(days, 33)
+      });
+      days++;
     }
 
     return biorhythmsData;
   };
+
+  function calcBioFormula(days, period) {
+    return Math.round(Math.sin(2 * Math.PI * (days / period)) * 100);
+  }
 });
